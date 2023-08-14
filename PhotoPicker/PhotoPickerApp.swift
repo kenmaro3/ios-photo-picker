9e14f84eb527cb2987fa816810fbcd9765040b4c
kenmaro3 <miha.ken.19@gmail.com>
2023-08-14 14:32:19 +0900

    Initial Commit

---
+++ PhotoPicker/PhotoPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoPickerApp.swift
+//  PhotoPicker
+//
+//  Created by Kentaro Mihara on 2023/08/14.
+//
+
+import SwiftUI
+
+@main
+struct PhotoPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
